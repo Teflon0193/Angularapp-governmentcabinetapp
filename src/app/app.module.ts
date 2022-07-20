be37611b01028280b651ad848a8078f05722cb51
unknown <admin@BV-W-ENG-ELE-DG.ADS.CPUT.AC.ZA>
2022-07-20 15:51:26 +0200

    deploy angular governmentapp to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MinisterService } from './minister.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [MinisterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
